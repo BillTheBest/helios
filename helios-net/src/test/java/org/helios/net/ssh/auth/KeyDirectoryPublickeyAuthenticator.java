5efc03400cc2ea306017701ace6d6ffe8a709dea
Nicholas Whitehead <nwhitehead@heliosdev.org>
2012-01-24 09:22:42 -0500

    Implemented public key loads into cache, indexed by user name.

--- helios-net/src/test/java/org/helios/net/ssh/auth/KeyDirectoryPublickeyAuthenticator.java
+++ helios-net/src/test/java/org/helios/net/ssh/auth/KeyDirectoryPublickeyAuthenticator.java
@@ -25,16 +25,19 @@
 package org.helios.net.ssh.auth;
 
 import java.io.File;
-import java.security.KeyPair;
 import java.security.PublicKey;
-import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.CopyOnWriteArraySet;
 
 import org.apache.log4j.Logger;
 import org.apache.sshd.common.keyprovider.FileKeyPairProvider;
 import org.apache.sshd.server.PublickeyAuthenticator;
 import org.apache.sshd.server.session.ServerSession;
-import org.helios.net.ssh.keys.AuthorizedKeysDecoder;
+import org.helios.net.ssh.keys.KeyDecoder;
+import org.helios.net.ssh.keys.UserAwarePublicKey;
+
 
 /**
  * <p>Title: KeyDirectoryPublickeyAuthenticator</p>
@@ -49,7 +52,7 @@ public class KeyDirectoryPublickeyAuthenticator implements PublickeyAuthenticato
 	/** The key pair provider */
 	protected final FileKeyPairProvider keyPairProvider;
 	/** The loaded public keys */
-	protected final Set<PublicKey> pks = new HashSet<PublicKey>();
+	protected final Map<String, Set<PublicKey>> pks = new ConcurrentHashMap<String, Set<PublicKey>>();
 	/** The instance logger */
 	protected final Logger log = Logger.getLogger(getClass());
 	
@@ -64,20 +67,30 @@ public class KeyDirectoryPublickeyAuthenticator implements PublickeyAuthenticato
 		if(keyDir==null) throw new IllegalArgumentException("The passed directory was null", new Throwable());
 		if(!keyDir.exists()) throw new IllegalArgumentException("The passed directory [" + keyDir + "] does not exist.", new Throwable());
 		if(!keyDir.isDirectory()) throw new IllegalArgumentException("The passed file [" + keyDir + "] is not a directory.", new Throwable());
-		this.keyDir = keyDir;				
-		AuthorizedKeysDecoder akd = new AuthorizedKeysDecoder();
+		this.keyDir = keyDir;	
+		int loaded = 0;
 		for(File f: keyDir.listFiles()) {
 			if(f.getName().contains("rsa") || f.getName().contains("dsa")) {
 				if(f.getName().toLowerCase().endsWith(".pub")) {
-					try {
-						pks.add(akd.decodePublicKey(f));
+					try {						
+						UserAwarePublicKey pubKey = KeyDecoder.getInstance().decodePublicKey(f);
+						Set<PublicKey> userPks = pks.get(pubKey.getUserName());
+						if(userPks==null) {
+							userPks = new CopyOnWriteArraySet<PublicKey>();
+							pks.put(pubKey.getUserName(), userPks);
+						}
+						userPks.add(pubKey.getPublicKey());
+						//log.info("Added [" + pubKey + "]");
 					} catch (Exception e) {
-						e.printStackTrace(System.err);
+						log.warn("Failed to load public key in file [" + f.getAbsolutePath() + "]", e);
 					}
 				}
 			}
 		}
-		log.info("Adding [" + pks.size() + "] keys to auth provider");
+		for(Set<PublicKey> upks: pks.values()) {
+			loaded += upks.size();
+		}
+		log.info("Loaded [" + loaded + "] keys for [" + pks.size() + "] users to auth provider");
 		keyPairProvider = null;
 	}
 
@@ -100,18 +113,16 @@ public class KeyDirectoryPublickeyAuthenticator implements PublickeyAuthenticato
 	public boolean authenticate(String username, PublicKey key, ServerSession session) {
 		if(username==null) throw new IllegalArgumentException("The passed user name was null", new Throwable());
 		if(key==null) throw new IllegalArgumentException("The passed key was null", new Throwable());
-		String keyType = key.getAlgorithm();
-		File keyFile = new File(keyDir.getAbsolutePath() + File.separator + username + "_" + keyType.toLowerCase() + ".pub");
-		log.info("Testing file [" + keyFile + "]");
-		if(!keyFile.exists()) return false;
-		PublicKey loadedKey = null;
-		try {
-			loadedKey = new AuthorizedKeysDecoder().decodePublicKey(keyFile);
-		} catch (Exception e) {
-			e.printStackTrace(System.err);
-			return false;
-		}			
-		return loadedKey.equals(key);
+		Set<PublicKey> userKeys = pks.get(username);
+		if(userKeys==null) {
+			synchronized(pks) {
+				userKeys = pks.get(username);
+				if(userKeys==null) {
+					return false;
+				}
+			}
+		}
+		return userKeys.contains(key);
 	}
 	
 //	protected byte[] getKeyBytes(File f) {
