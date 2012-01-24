a00a89aa458530adf24efadada1ba9c475c32603
Nicholas Whitehead <nwhitehead@heliosdev.org>
2012-01-24 17:09:09 -0500

    Added public key authentication tests.

--- helios-net/src/test/java/org/helios/net/ssh/auth/SSHAuthenticationTestCase.java
+++ helios-net/src/test/java/org/helios/net/ssh/auth/SSHAuthenticationTestCase.java
@@ -25,6 +25,8 @@
 package org.helios.net.ssh.auth;
 
 import java.io.File;
+import java.io.FilenameFilter;
+import java.util.HashMap;
 import java.util.Map;
 
 import org.apache.log4j.Logger;
@@ -54,10 +56,17 @@ public class SSHAuthenticationTestCase {
 	@Rule
     public TestName testName= new TestName();
 	/** Instance logger */
-	protected final Logger LOG = Logger.getLogger(getClass());
+	protected static final Logger LOG = Logger.getLogger(SSHAuthenticationTestCase.class);
 	
 	/** Known good credentials  */
 	static Map<Object, Object> goodPasswordAuths = FileHelper.loadProperties(new File("./src/test/resources/auth/password/credentials.properties"));
+	/** Private Key Passphrases  */
+	static Map<Object, Object> pkPassphrases = FileHelper.loadProperties(new File("./src/test/resources/auth/keys/passphrases.properties"));
+	/** SSA Private Key Files keyed by user name */
+	static Map<String, File> dsaPks = new HashMap<String, File>();	
+	/** RSA Private Key Files keyed by user name */
+	static Map<String, File> rsaPks = new HashMap<String, File>();
+	
 	
 	/**
 	 * Starts the test SSHD server
@@ -73,6 +82,25 @@ public class SSHAuthenticationTestCase {
 				try { ApacheSSHDServer.stop(true); } catch (Exception e) {} 
 			}
 		});
+		// Load the RSA private keys
+		for(File pk: new File("./src/test/resources/auth/keys").listFiles(new FilenameFilter(){
+				public boolean accept(File dir, String name) {
+					return name.endsWith("_rsa");
+				}
+			})) {
+			rsaPks.put(pk.getName().replace("_rsa", ""), pk);
+		}
+		LOG.info("Loaded [" + rsaPks.size() + "] RSA Private Keys");
+		// Load the DSA private keys
+		for(File pk: new File("./src/test/resources/auth/keys").listFiles(new FilenameFilter(){
+				public boolean accept(File dir, String name) {
+					return name.endsWith("_dsa");
+				}
+			})) {
+			dsaPks.put(pk.getName().replace("_dsa", ""), pk);
+		}
+		LOG.info("Loaded [" + dsaPks.size() + "] DSA Private Keys");
+		
 	}
 
 	/**
@@ -155,20 +183,23 @@ public class SSHAuthenticationTestCase {
 	 */
 	@Test()
 	public void testSharedPasswordSSHLogin() throws Exception {
-		ApacheSSHDServer.activatePasswordAuthenticator(true);
+		ApacheSSHDServer.activatePasswordAuthenticator(true);		
 		Map.Entry<Object, Object> creds = goodPasswordAuths.entrySet().iterator().next();
-		SSHService ssh = SSHService.createSSHService("localhost", SSHD_PORT, creds.getKey().toString())
-				.sshUserPassword(creds.getValue().toString())
+		SSHService ssh = SSHService.createSSHService("localhost", SSHD_PORT, creds.getKey().toString())				
 				.connect()
+				.sshUserPassword(creds.getValue().toString())
 				.authenticate();
 		Assert.assertEquals("The SSHService1 shared count", 1, ssh.getSharedCount());
+		Assert.assertTrue("The SSHService1 is shared connection", ssh.isSharedConnection());
 		Assert.assertTrue("The SSHService1 is connected", ssh.isConnected());
 		Assert.assertTrue("The SSHService1 is authenticated", ssh.isAuthenticated());
 		
-		SSHService ssh2 = SSHService.createSSHService("localhost", SSHD_PORT, creds.getKey().toString())
+		SSHService ssh2 = SSHService.createSSHService("localhost", SSHD_PORT, creds.getKey().toString())				
+				.connect()
 				.sshUserPassword(creds.getValue().toString())
-				.connect();
-		Assert.assertEquals("The SSHService2 shared count", 2, ssh2.getSharedCount());		
+				.authenticate();
+		Assert.assertEquals("The SSHService2 shared count", 2, ssh2.getSharedCount());	
+		Assert.assertTrue("The SSHService2 is shared connection", ssh2.isSharedConnection());
 		Assert.assertTrue("The SSHService2 is connected", ssh2.isConnected());
 		Assert.assertTrue("The SSHService2 is authenticated", ssh2.isAuthenticated());
 
@@ -198,5 +229,216 @@ public class SSHAuthenticationTestCase {
 		
 	}
 	
+	/**
+	 * Tests authentication with known good username/password
+	 * @throws Exception
+	 */
+	@Test()
+	public void testExclusivePasswordSSHLogin() throws Exception {
+		ApacheSSHDServer.activatePasswordAuthenticator(true);
+		Map.Entry<Object, Object> creds = goodPasswordAuths.entrySet().iterator().next();		
+		SSHService ssh = SSHService.createSSHService("localhost", SSHD_PORT, creds.getKey().toString(), false, false)				
+				.connect()
+				.sshUserPassword(creds.getValue().toString())
+				.authenticate();
+		Assert.assertEquals("The SSHService1 shared count", 1, ssh.getSharedCount());
+		Assert.assertFalse("The SSHService1 is shared connection", ssh.isSharedConnection());
+		Assert.assertTrue("The SSHService1 is connected", ssh.isConnected());
+		Assert.assertTrue("The SSHService1 is authenticated", ssh.isAuthenticated());
+		
+		SSHService ssh2 = SSHService.createSSHService("localhost", SSHD_PORT, creds.getKey().toString(), false, false)				
+				.connect()
+				.sshUserPassword(creds.getValue().toString())
+				.authenticate();
+		Assert.assertEquals("The SSHService2 shared count", 1, ssh2.getSharedCount());
+		Assert.assertFalse("The SSHService2 is shared connection", ssh2.isSharedConnection());
+		Assert.assertTrue("The SSHService2 is connected", ssh2.isConnected());
+		Assert.assertTrue("The SSHService2 is authenticated", ssh2.isAuthenticated());
 
+		SSHService ssh3 = SSHService.createSSHService("localhost", SSHD_PORT, creds.getKey().toString())				
+				.connect()
+				.sshUserPassword(creds.getValue().toString())
+				.authenticate();
+		Assert.assertEquals("The SSHService3 shared count", 1, ssh3.getSharedCount());
+		Assert.assertTrue("The SSHService3 is shared connection", ssh3.isSharedConnection());
+		Assert.assertTrue("The SSHService3 is connected", ssh3.isConnected());
+		Assert.assertTrue("The SSHService3 is authenticated", ssh3.isAuthenticated());
+		
+		SSHService ssh4 = SSHService.createSSHService("localhost", SSHD_PORT, creds.getKey().toString())				
+				.connect()
+				.sshUserPassword(creds.getValue().toString())
+				.authenticate();
+		Assert.assertEquals("The SSHService4 shared count", 2, ssh4.getSharedCount());
+		Assert.assertTrue("The SSHService4 is shared connection", ssh4.isSharedConnection());
+		Assert.assertTrue("The SSHService4 is connected", ssh4.isConnected());
+		Assert.assertTrue("The SSHService4 is authenticated", ssh4.isAuthenticated());
+		
+		ssh3.close();
+		
+		Assert.assertEquals("The SSHService3 shared count", 1, ssh3.getSharedCount());
+		Assert.assertTrue("The SSHService3 is connected", ssh3.isConnected());
+		Assert.assertTrue("The SSHService3 is authenticated", ssh3.isAuthenticated());
+		
+		Assert.assertEquals("The SSHService4 shared count", 1, ssh4.getSharedCount());
+		Assert.assertTrue("The SSHService4 is connected", ssh4.isConnected());
+		Assert.assertTrue("The SSHService4 is authenticated", ssh4.isAuthenticated());
+		
+		ssh4.close();
+
+		Assert.assertEquals("The SSHService3 shared count", 0, ssh3.getSharedCount());
+		Assert.assertFalse("The SSHService3 is connected", ssh3.isConnected());
+		Assert.assertFalse("The SSHService3 is authenticated", ssh3.isAuthenticated());
+		
+		Assert.assertEquals("The SSHService4 shared count", 0, ssh4.getSharedCount());
+		Assert.assertFalse("The SSHService4 is connected", ssh4.isConnected());
+		Assert.assertFalse("The SSHService4 is authenticated", ssh4.isAuthenticated());
+		
+		ssh2.close();
+		Assert.assertEquals("The SSHService2 shared count", 0, ssh2.getSharedCount());
+		Assert.assertFalse("The SSHService2 is connected", ssh2.isConnected());
+		Assert.assertFalse("The SSHService2 is authenticated", ssh2.isAuthenticated());
+		
+		ssh.close();
+		Assert.assertEquals("The SSHService1 shared count", 0, ssh.getSharedCount());
+		Assert.assertFalse("The SSHService1 is connected", ssh.isConnected());
+		Assert.assertFalse("The SSHService1 is authenticated", ssh.isAuthenticated());
+		
+	}
+	
+	/**
+	 * Tests authentication with RSA public/private key and known good passphrase
+	 * @throws Exception
+	 */
+	@Test()
+	public void testSimpleRSAPublicKeySSHLogin() throws Exception {		
+		ApacheSSHDServer.activateKeyAuthenticator(true);
+		int usersTested = 0;
+		for(Map.Entry<Object, Object> pkp: pkPassphrases.entrySet()) {
+			if(pkp.getValue()==null || pkp.getValue().toString().trim().isEmpty()) continue;
+			String userName = pkp.getKey().toString();
+			String passphrase = pkp.getValue().toString();
+			File pkFile = rsaPks.get(userName);
+			
+			SSHService ssh = SSHService.createSSHService("localhost", SSHD_PORT, userName)				
+					.connect()
+					.sshPassphrase(passphrase)
+					.pemPrivateKeyFile(pkFile)					
+					.authenticate();
+			Assert.assertEquals("The SSHService1 [" + userName + "] shared count", 1, ssh.getSharedCount());
+			Assert.assertTrue("The SSHService1 [" + userName + "] is shared connection", ssh.isSharedConnection());
+			Assert.assertTrue("The SSHService1 [" + userName + "] is connected", ssh.isConnected());
+			Assert.assertTrue("The SSHService1 [" + userName + "] is authenticated", ssh.isAuthenticated());
+			
+			ssh.close();
+
+			Assert.assertEquals("The SSHService1 [" + userName + "] shared count", 0, ssh.getSharedCount());
+			Assert.assertFalse("The SSHService1 [" + userName + "] is connected", ssh.isConnected());
+			Assert.assertFalse("The SSHService1 [" + userName + "] is authenticated", ssh.isAuthenticated());
+						
+			usersTested++;
+		}
+		Assert.assertTrue("Tested at least 2 users", usersTested > 1);
+	}
+	
+	/**
+	 * Tests authentication with SSA public/private key and known good passphrase
+	 * @throws Exception
+	 */
+	@Test()
+	public void testSimpleDSAPublicKeySSHLogin() throws Exception {		
+		ApacheSSHDServer.activateKeyAuthenticator(true);
+		int usersTested = 0;
+		for(Map.Entry<Object, Object> pkp: pkPassphrases.entrySet()) {
+			if(pkp.getValue()==null || pkp.getValue().toString().trim().isEmpty()) continue;
+			String userName = pkp.getKey().toString();
+			String passphrase = pkp.getValue().toString();
+			File pkFile = dsaPks.get(userName);
+			
+			SSHService ssh = SSHService.createSSHService("localhost", SSHD_PORT, userName)				
+					.connect()
+					.sshPassphrase(passphrase)
+					.pemPrivateKeyFile(pkFile)					
+					.authenticate();
+			Assert.assertEquals("The SSHService1 [" + userName + "] shared count", 1, ssh.getSharedCount());
+			Assert.assertTrue("The SSHService1 [" + userName + "] is shared connection", ssh.isSharedConnection());
+			Assert.assertTrue("The SSHService1 [" + userName + "] is connected", ssh.isConnected());
+			Assert.assertTrue("The SSHService1 [" + userName + "] is authenticated", ssh.isAuthenticated());
+			
+			ssh.close();
+
+			Assert.assertEquals("The SSHService1 [" + userName + "] shared count", 0, ssh.getSharedCount());
+			Assert.assertFalse("The SSHService1 [" + userName + "] is connected", ssh.isConnected());
+			Assert.assertFalse("The SSHService1 [" + userName + "] is authenticated", ssh.isAuthenticated());
+						
+			usersTested++;
+		}
+		Assert.assertTrue("Tested at least 2 users", usersTested > 1);
+	}
+	
+	/**
+	 * Tests authentication with RSA public/private key with no passphrase
+	 * @throws Exception
+	 */
+	@Test()
+	public void testSimpleNoPassPhraseRSAPublicKeySSHLogin() throws Exception {		
+		ApacheSSHDServer.activateKeyAuthenticator(true);
+		int usersTested = 0;
+		for(Map.Entry<Object, Object> pkp: pkPassphrases.entrySet()) {
+			if(!pkp.getValue().toString().trim().isEmpty()) continue;
+			String userName = pkp.getKey().toString();			
+			File pkFile = rsaPks.get(userName);
+			SSHService ssh = SSHService.createSSHService("localhost", SSHD_PORT, userName)				
+					.connect()
+					.pemPrivateKeyFile(pkFile)					
+					.authenticate();
+			Assert.assertEquals("The SSHService1 [" + userName + "] shared count", 1, ssh.getSharedCount());
+			Assert.assertTrue("The SSHService1 [" + userName + "] is shared connection", ssh.isSharedConnection());
+			Assert.assertTrue("The SSHService1 [" + userName + "] is connected", ssh.isConnected());
+			Assert.assertTrue("The SSHService1 [" + userName + "] is authenticated", ssh.isAuthenticated());
+			
+			ssh.close();
+
+			Assert.assertEquals("The SSHService1 [" + userName + "] shared count", 0, ssh.getSharedCount());
+			Assert.assertFalse("The SSHService1 [" + userName + "] is connected", ssh.isConnected());
+			Assert.assertFalse("The SSHService1 [" + userName + "] is authenticated", ssh.isAuthenticated());
+						
+			usersTested++;
+		}
+		Assert.assertTrue("Tested at least 1 user", usersTested > 0);
+	}
+	
+	/**
+	 * Tests authentication with DSA public/private key with no passphrase
+	 * @throws Exception
+	 */
+	@Test()
+	public void testSimpleNoPassPhraseDSAPublicKeySSHLogin() throws Exception {		
+		ApacheSSHDServer.activateKeyAuthenticator(true);
+		int usersTested = 0;
+		for(Map.Entry<Object, Object> pkp: pkPassphrases.entrySet()) {
+			if(!pkp.getValue().toString().trim().isEmpty()) continue;
+			String userName = pkp.getKey().toString();			
+			File pkFile = dsaPks.get(userName);
+			SSHService ssh = SSHService.createSSHService("localhost", SSHD_PORT, userName)				
+					.connect()
+					.pemPrivateKeyFile(pkFile)					
+					.authenticate();
+			Assert.assertEquals("The SSHService1 [" + userName + "] shared count", 1, ssh.getSharedCount());
+			Assert.assertTrue("The SSHService1 [" + userName + "] is shared connection", ssh.isSharedConnection());
+			Assert.assertTrue("The SSHService1 [" + userName + "] is connected", ssh.isConnected());
+			Assert.assertTrue("The SSHService1 [" + userName + "] is authenticated", ssh.isAuthenticated());
+			
+			ssh.close();
+
+			Assert.assertEquals("The SSHService1 [" + userName + "] shared count", 0, ssh.getSharedCount());
+			Assert.assertFalse("The SSHService1 [" + userName + "] is connected", ssh.isConnected());
+			Assert.assertFalse("The SSHService1 [" + userName + "] is authenticated", ssh.isAuthenticated());
+						
+			usersTested++;
+		}
+		Assert.assertTrue("Tested at least 1 user", usersTested > 0);
+	}
+	
+
+	
 }
