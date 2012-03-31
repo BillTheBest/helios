27dd5ea613990ed003dd96c07d1286ad4530480d
Nicholas Whitehead <nwhitehead@heliosdev.org>
2012-03-31 06:15:03 -0400

    Updated registration of event type org.helios.ot.generic.IGenericMetric. Changed launcher name in otserver.sh and added commented debug options.

--- helios-ot-server/otserver.sh
+++ helios-ot-server/otserver.sh
@@ -10,7 +10,7 @@
 export M2_REPO=~/.m2/repository
 #export JAVA_HOME=/usr/lib/jvm/java-6-sun
 export EXE_JAR=$M2_REPO/org/helios/helios-spring/helios-spring-launcher/1.0-SNAPSHOT/helios-spring-launcher-1.0-SNAPSHOT.jar
-export CMD_LINE="-el ./src/test/resources/server/conf/OTServer.launch -conf ./src/test/resources/server/conf -log4j ./src/test/resources/server/conf/log4j/log4j.xml -daemon "
+export CMD_LINE="-el ./src/test/resources/server/conf/DevOTServer.launch -conf ./src/test/resources/server/conf -log4j ./src/test/resources/server/conf/log4j/log4j.xml -daemon "
 #export CMD_LINE="$CMD_LINE -lib $M2_REPO/org/slf4j/slf4j-log4j12/1.5.8 "
 export CMD_LINE="$CMD_LINE -lib $M2_REPO/org/helios/helios-ot-server/1.0-SNAPSHOT "
 export CMD_LINE="$CMD_LINE -lib $M2_REPO/org/helios/helios-collectors/helios-collectors-core/1.0-SNAPSHOT "
