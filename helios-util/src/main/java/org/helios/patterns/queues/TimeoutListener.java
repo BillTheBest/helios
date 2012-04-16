98bf48c6e2df3e2205e12985a41463d086b3b15f
Nicholas Whitehead <nwhitehead@heliosdev.org>
2012-04-16 06:46:58 -0400

    Adding timeout entry map implementation.

---
+++ helios-util/src/main/java/org/helios/patterns/queues/TimeoutListener.java
@@ -0,0 +1,42 @@
+/**
+ * Helios, OpenSource Monitoring
+ * Brought to you by the Helios Development Group
+ *
+ * Copyright 2007, Helios Development Group and individual contributors
+ * as indicated by the @author tags. See the copyright.txt file in the
+ * distribution for a full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org. 
+ *
+ */
+package org.helios.patterns.queues;
+
+/**
+ * <p>Title: TimeoutListener</p>
+ * <p>Description: Defines a listener that is notified of delayed value timeouts</p> 
+ * <p>Company: Helios Development Group LLC</p>
+ * @author Whitehead (nwhitehead AT heliosdev DOT org)
+ * <p><code>org.helios.patterns.queues.TimeoutListener</code></p>
+ */
+
+public interface TimeoutListener<K, V> {
+	/**
+	 * Callback when a delayed value times out
+	 * @param key The key of the dleayed value
+	 * @param value The delayed value
+	 */
+	public void onTimeout(K key, V value);
+}
