daba5de903ac729a78fbcaf160e19907f892243b
Sandeep Malhotra <smalhotra@heliosdev.org>
2012-06-22 18:36:22 -0400

    Minor changes

--- helios-collectors/collectors-core/src/main/java/org/helios/collectors/jdbc/ResultSetMap.java
+++ helios-collectors/collectors-core/src/main/java/org/helios/collectors/jdbc/ResultSetMap.java
@@ -19,7 +19,7 @@
  * You should have received a copy of the GNU Lesser General Public
  * License along with this software; if not, write to the Free
  * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
- * 02110-1301 USA, or see the FSF site: http://www.fsf.org. 
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
  *
  */
 package org.helios.collectors.jdbc;
@@ -33,7 +33,7 @@ import org.w3c.dom.Node;
 
 /**
  * <p>Title: ResultSetMap</p>
- * <p>Description: A syntax parser for defining a subsidiary mapping within a <code>SQLMapping<code>.</p> 
+ * <p>Description: A syntax parser for defining a subsidiary mapping within a <code>SQLMapping<code>.</p>
  * <p>Company: Helios Development Group</p>
  * @author Whitehead (whitehead.nicholas@gmail.com)
  * @version $LastChangedRevision$
@@ -67,103 +67,20 @@ public class ResultSetMap {
 	protected boolean scoped = false;
 	/** The scope value */
 	protected Object scopeValue = null;
-	
+
 	/**
 	 * @param rsetMapNode
 	 */
 	public ResultSetMap(Node rsetMapNode) {
-		
+
 	}
-	
+
 	/**
 	 * Extracts and traces metrics from the processed result set.
 	 * @param prs the IProcessedResultSet to extract metrics from.
 	 */
 	public void trace(IProcessedResultSet prs) {
-		
+
 	}
-	
-	/**
-	 * Sample Query Output
-	 * 		SCHEMA_NAME, RELNAME, SEQ_SCAN, TUPLE_READS
-			"pg_catalog";"pg_class";25268;5697968
-			"pg_catalog";"pg_database";12261;61245
-			"pg_catalog";"pg_am";8416;8416
-			"pg_catalog";"pg_attribute";56;304
-			"pg_catalog";"pg_amop";8;40
 
-	 */
-	
-	/*
-	 		
-	 */
-	
-	
-	// Tokens:
-		// RSET Independent
-			// Context NVP
-			// Cache Value
-			// Spring Bean Value
-			// JMX MBean Attribute Value
-		// RSET Dependent
-			// Col Name
-			// Col Value
-			// Flat Col Value
-	
-	
-	/*
-	 * <Mapping column="1" segment="Server Logins|{0}" metricName="Count" type="CINT"/>
-	 * <Mapping column="1" segment="Server Logins|{0}" metricName="Count" type="CINT"/>
-	 * <Mapping column="1" segment="09" metricName="HighWaterMark" type="STRING" attributeName="HighWaterMark09" attributeType="String"/>
-	 * 
-	 			<Bind number="1" source="attribute" type="String" objectName="com.adp.sbs.metrics:service=OCLHighWaterMarks" attributeName="HighWaterMark09"/>
-				<Bind number="2" source="attribute" type="String" objectName="com.adp.sbs.metrics:service=OCLHighWaterMarks" attributeName="HighWaterMark09"/>					
-				<Bind number="3" source="attribute" type="String" objectName="com.adp.sbs.metrics:service=OCLHighWaterMarks" attributeName="HighWaterMark09"/>
-				<Bind number="4" source="attribute" type="String" objectName="com.adp.sbs.metrics:service=OCLHighWaterMarks" attributeName="HighWaterMark09"/>
-
-		<Mapping column="0" segment="" metricName="Total Blocked Sessions" type="CINT"  scoped="true" scopeResetValue="0" />
-		
-						<CacheResult
-							objectName="com.adp.sbs.monitoring:type=CachedResultSets,name=DC1"
-							attributeName="BlockingLocks"
-							ContainerClass="com.adp.sbs.metrics.tracing.collectors.jdbc.cache.HistoryBufferingCachedResultSet2">
-							<attribute name="HistorySize">20</attribute>
-						</CacheResult>
-
-					<CacheResult
-						objectName="com.adp.sbs.monitoring:type=CachedResultSets,name=DC1"
-						attributeName="OracleConnectionsByServer"
-						ContainerClass="com.adp.sbs.metrics.tracing.collectors.jdbc.cache.HistoryBufferingCachedResultSet2">
-						<attribute name="HistorySize">1</attribute>
-					</CacheResult>
-					
-					
-					
-					
-import java.text.*;
-
-public String[][] postProcess(String[][] input) {
-	DecimalFormat df = new DecimalFormat("###,###,###");	
-	int rowCnt = 0;	
-	input.each() { row ->
-		int colCnt = 0;
-		row.each() { column ->
-			if(!column.getClass().getName().equals("java.lang.String")) {				
-				input[rowCnt][colCnt] = "&nbsp;";				
-			} else {
-				try {
-					Number number = df.parse(column);
-					input[rowCnt][colCnt] = df.format(number.doubleValue());
-				} catch (Exception e) {}
-			}
-			colCnt++;
-		}
-		rowCnt++;
-	}
-	//Thread.currentThread().sleep(200);
-	return input;
-}					
-		
-
-	 */
 }
--- helios-collectors/src/main/java/org/helios/collectors/jmx/tracers/ServiceMBeanStateFormatter.java
+++ helios-collectors/src/main/java/org/helios/collectors/jmx/tracers/ServiceMBeanStateFormatter.java
@@ -19,7 +19,7 @@
  * You should have received a copy of the GNU Lesser General Public
  * License along with this software; if not, write to the Free
  * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
- * 02110-1301 USA, or see the FSF site: http://www.fsf.org. 
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
  *
  */
 package org.helios.collectors.jmx.tracers;
@@ -28,8 +28,8 @@ import org.helios.collectors.jmx.AbstractObjectFormatter;
 
 /**
  * <p>Title: ServiceMBeanStateTracer</p>
- * <p>Description: Simple Tracer to interpret whether an 
- * MBean is online based on value for attribute state returned by an MBean Server</p> 
+ * <p>Description: Simple Tracer to interpret whether an
+ * MBean is online based on value for attribute state returned by an MBean Server</p>
  * <p>Company: Helios Development Group</p>
  * @author Whitehead (whitehead.nicholas@gmail.com)
  */
@@ -37,16 +37,15 @@ public class ServiceMBeanStateFormatter extends AbstractObjectFormatter {
 
 	/**
 	 * Reads a string value, converts to an int, then returns 1 if the value is 3 and 0 for all other values.
-	 * @param obj An object presumed to be an Integer. 
-	 * @return A 1 if the service is <code>Started</code> (3) or 0. 
-	 * @see com.adp.sbs.metrics.tracing.collectors.jmx.IObjectFormatter#renderTracingValue(java.lang.Object)
+	 * @param obj An object presumed to be an Integer.
+	 * @return A 1 if the service is <code>Started</code> (3) or 0.
 	 */
 	public String format(Object obj) {
 		try {
 			Integer state = (Integer)obj;
-			if(state.intValue()==3) 
+			if(state.intValue()==3)
 				return "1";
-			else 
+			else
 				return "0";
 		} catch (Exception e) {
 			return "0";
